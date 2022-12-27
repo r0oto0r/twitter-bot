9ba342dc3d260348381b4861825ee832c0c807bc
Ben <ben@forkbomb.dev>
2022-12-27 02:26:17 +0100

    Fixed timezone issue in profile timestamp.

--- src/Mastodon.ts
+++ src/Mastodon.ts
@@ -118,7 +118,7 @@ export class Mastodon {
 				},
 				{
 					name: 'Last Sync',
-					value: moment().local().format('DD.MM.YY - HH:mm:ss')
+					value: moment().utc().local().format('DD.MM.YY - HH:mm:ss')
 				}
 			]
 		});
